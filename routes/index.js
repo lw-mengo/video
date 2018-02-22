857e2288c070cbdd42a1601503682105605bcf62
Administrator <867740815@qq.com>
2018-02-22 22:03:56 +0800

    some problems

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,14 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    request("http://localhost:8080/videoapi/getIndexApi", function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body);
-            var data = JSON.parse(body);
-            res.render("index",{index_data:data});
-        }
-    });
+    // request("http://localhost:8080/videoapi/getIndexApi", function (error, response, body) {
+    //     if (!error && response.statusCode == 200) {
+    //         console.log(body);
+    //         var data = JSON.parse(body);
+    //         res.render("index",{index_data:data});
+    //     }
+    // });
+    res.render("index");
 
 });
 
