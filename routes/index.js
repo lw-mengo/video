e937136849082bc259e9653ef09f0ae999f04c6f
lw_mengo <867740815@qq.com>
2018-02-22 17:26:15 +0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    request("http://localhost:8080/videoapi/getIndexApi", function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+            var data = JSON.parse(body);
+            res.render("index",{index_data:data});
+        }
+    });
+
+});
+
+module.exports = router;
