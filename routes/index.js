54f923d981cf84d9c15e9b366939abde70c59a10
lw_mengo <867740815@qq.com>
2018-03-05 16:20:23 +0800

    初步解决了一下多个api数据的整合

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var rp = require('request-promise');
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -13,31 +14,95 @@ router.get('/', function (req, res) {
     // });
 
     var url = "https://api.bgm.tv/calendar";
-    request(url,function (error,response,body) {
-       if (!error&&response.statusCode==200){
-           var data = JSON.parse(body);
-           res.render("index",{mydata:data});
-       }
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            res.render("index", {mydata: data});
+        }
     });
-   // res.render("index");
+    // res.render("index");
 
 });
 
-router.get("/test",function (req,res) {
+router.get("/test", function (req, res) {
     res.render("test");
 });
 
-router.get("/info",function (req,res) {
-    var subject =req.query.subject;
-    console.log(subject);
-    var base_url ="https://api.bgm.tv/subject/"+subject;
-    request(base_url,function (error,response,body) {
-        if (!error&&response.statusCode==200){
-            var data = JSON.parse(body);
-            res.render("bangumi_info",{bangumi_info:data});
+// function getInfo(url) {
+//      return rp(url).then(function (value) {
+//         //console.log(value);
+//         //res.render("bangumi_info",{bangumi_info:value});
+//         return value;
+//     }).catch(function (reason) {
+//         console.log(reason);
+//     }).then(rp);
+// };
+
+// function getInfo(uri,uri2,m,n) {
+//     rp(uri).then(function (value) {
+//         m = value;
+//     }).then(rp(uri2).then(function (value) {
+//         n = value;
+//     })).catch(function (reason) {
+//
+//     });
+//
+//     return ;
+// }
+
+router.get("/info", function (req, res) {
+    var subject = req.query.subject;
+    var options = {
+        uri: "https://api.bgm.tv/subject/" + subject,
+        method: "GET",
+        json: true
+    };
+    var options_1 = {
+        uri: "https://api.bgm.tv/subject/" + subject + "/ep",
+        method: "GET",
+        json: true
+    };
+    var bangumi_info,ep_info;
+    rp(options).then(function (value) {
+        bangumi_info = value;
+        console.log(value);
+        //res.render("bangumi_info",{bangumi_info:value});
+        //return value;
+    }).then(rp(options_1).then(function (value) {
+        ep_info = value;
+        console.log(value);
+        res.render("bangumi_info",{bangumi_info:bangumi_info,ep:ep_info});
         }
+    )).catch(function (reason) {
+        console.log(reason);
     });
- //   res.render("bangumi_info");
+    /**优化一下代码**/
+
+
+
+
+
+
+    //  rp(url).then(function (value) {
+    //    console.log(value);
+    //    res.render("bangumi_info",{bangumi_info:value});
+    //    next();
+    // }).catch(function (reason) {
+    //     console.log(reason);
+    // });
+
+
+    // var subject =req.query.subject;
+    // console.log(subject);
+    // var base_url ="https://api.bgm.tv/subject/"+subject;
+    // request(base_url,function (error,response,body) {
+    //     if (!error&&response.statusCode==200){
+    //         var data = JSON.parse(body);
+    //         res.render("bangumi_info",{bangumi_info:data});
+    //     }
+    // });
+    //res.render("bangumi_info");
+
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,45 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+/*测试下回调函数数组处理路由*/
+//
+// var cb0 = function (req,res,next) {
+//     console.log("CB-0");
+//     var hello = "hello";
+//     res.send(hello);
+//     return hello;
+//     next();
+// };
+// var cb1 = function (req,res,next) {
+//     console.log("CB-1");
+//     var world = "world";
+//     return world;
+//     next();
+// };
+// var cb2 = function (req,res) {
+//     console.log("cb-3");
+//     res.send("hello world!");
+// };
+// router.get('/my_test',[cb0,cb1,cb2]);
+//
+//
+
+// router.use(function timeLog(req,res,next) {
+//     console.log("time",Date.now());
+//     next();
+// });
+// router.get("/my_test",function (req,res) {
+//     res.send("Birds home page");
+// });
+router.get("/my_test/:id",function (req,res,next) {
+   console.log("ID",req.params.id);
+   next();
+},function (req,res,next) {
+    res.send("User Info");
+});
+
+router.get("/my_test/:id",function (req,res,next) {
+   res.end(req.params.id);
+});
 
 module.exports = router;
